ed0ae168346fe3d7776600405ebd22113448be2e
tai <nguyenthetai96@gmail.com>
2018-10-05 14:44:33 +0700

    AVON-210 Grant item lightbox design and layout

--- src/main/webapp/app/grants/description-grant/description-grant.component.ts
+++ src/main/webapp/app/grants/description-grant/description-grant.component.ts
@@ -3,6 +3,7 @@ import {NgbActiveModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {ActivatedRoute} from '@angular/router';
 import {DescriptionGrantModalService} from './description-grant-modal.service';
 import {GrantDTO} from '../../shared';
+import {EventManager, JhiLanguageService} from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-description-grant-modal',
@@ -15,10 +16,26 @@ export class JhiDescriptionGrantModalComponent implements OnInit {
     grantDTO: GrantDTO;
     formatDate = 'MM/dd/yyyy';
 
-    constructor(public activeModal: NgbActiveModal) {
+    constructor(public activeModal: NgbActiveModal,
+                private languageService: JhiLanguageService,
+                private eventManager: EventManager) {
     }
 
     ngOnInit() {
+        this.languageService.getCurrent().then((current) => {
+            if (current === 'en') {
+                this.formatDate = 'MM/dd/yyyy';
+            } else if (current === 'sv') {
+                this.formatDate = 'yyyy/MM/dd';
+            }
+        });
+        this.eventManager.subscribe('changeLanguage', (response) => {
+            if (response.content === 'en') {
+                this.formatDate = 'MM/dd/yyyy';
+            } else if (response.content === 'sv') {
+                this.formatDate = 'yyyy/MM/dd';
+            }
+        });
     }
 
     openNewTab() {
@@ -28,6 +45,18 @@ export class JhiDescriptionGrantModalComponent implements OnInit {
 
     clear() {
         this.activeModal.dismiss('cancel');
+    }
+
+    getFinanceDescription(grantDTO) {
+        if (grantDTO.grantProgramDTO.publisherDTO.id !== 9) {
+            return grantDTO.financeDescription.replace(/<[^>]*>/g, '');
+        } else {
+            if (isNaN(grantDTO.financeDescription)) {
+                return grantDTO.financeDescription.replace(/<[^>]*>/g, '');
+            } else {
+                return Number(Number(grantDTO.financeDescription).toFixed(1)).toLocaleString() + ' EUR';
+            }
+        }
     }
 }
 
